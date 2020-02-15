018a878552b2f977380fe5c3587e44f0cc2b459b
Unknown <jan@lohmannhannover.de>
2020-02-15 09:23:51 +0100

    added basic mesh delta obj

--- js/init.js
+++ js/init.js
@@ -15,8 +15,25 @@ var createScene = function () {
     var light1 = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(1, 1, 0), scene);
     var light2 = new BABYLON.PointLight("light2", new BABYLON.Vector3(0, 1, -1), scene);
 
+    // The first parameter can be used to specify which mesh to import. Here we import all meshes
+    BABYLON.SceneLoader.ImportMesh("", "./assets/models", "Delta.obj", scene, function (newMeshes) {
+        // Set the target of the camera to the first imported mesh
+        camera.target = newMeshes[0];
+    });
+
     // Add and manipulate meshes in the scene
     var sphere = BABYLON.MeshBuilder.CreateSphere("sphere", { diameter: 4 }, scene);
+
+    // Create a material with our land texture.
+    var groundMaterial = new BABYLON.StandardMaterial("ground", scene);
+    groundMaterial.diffuseTexture = new BABYLON.Texture("./assets/textures/ground.jpg", scene);
+
+    // This shows how we would apply this material to a plane. In our later
+    // example we'll replace this with CreateGroundFromHeightMap.
+    var groundPlane = BABYLON.Mesh.CreatePlane("groundPlane", 200.0, scene);
+
+    // When our new mesh is read, apply our material.
+    groundPlane.material = groundMaterial;
 
     return scene;
 };
