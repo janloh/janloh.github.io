f4407a9dfc8ad9f5d55a28b5dea738942066d08e
Unknown <jan@lohmannhannover.de>
2020-02-17 22:10:22 +0100

    limit zoom

--- js/init.js
+++ js/init.js
@@ -9,11 +9,13 @@ var createScene = function () {
     scene.clearColor = BABYLON.Color3.Gray();
 
     // Add a camera to the scene and attach it to the canvas
-    var camera = new BABYLON.ArcRotateCamera("Camera", -Math.PI / 2, 3 * (Math.PI / 4), 2, BABYLON.Vector3.Zero(), scene);
+    var camera = new BABYLON.ArcRotateCamera("Camera", -Math.PI / 2, 3 * (Math.PI / 4), 10, BABYLON.Vector3.Zero(), scene);
     camera.setPosition(new BABYLON.Vector3(-5, 5, 0));
     camera.lowerBetaLimit = 0.1;
     camera.upperBetaLimit = (Math.PI / 2) * 0.95;
     camera.attachControl(canvas, true);
+    camera.lowerRadiusLimit = 5;
+    camera.upperRadiusLimit = 15;
 
     // Add lights to the scene
     var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
