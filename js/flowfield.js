e6a2b04acb65ea89977cc725263159edc50af827
Jan <jan@lohmannhannover.de>
2021-03-23 15:42:38 +0100

    getting flowfield to run

--- js/flowfield.js
+++ js/flowfield.js
@@ -1,52 +1,8 @@
-/* Based on the work of: Johan Karlsson and Tibix */
+/* Based on the work of: Johan Karlsson and https://codepen.io/Tibixx */
 
-let canvas, ctx, field, width, height, fieldSize, columns, rows, noiseZ, particles, hue;
-(noiseZ = 0);
-particleCount = 2000;
-particleSize = 0.9;
-fieldSize = 70;
-fieldForce = 0.15;
-noiseSpeed = 0.003;
-sORp = true;
-trailLength = 0.15;
-hueBase = 10;
-hueRange = 5;
-maxSpeed = 2.5;
-enableGUI = true;
-
-var ui = new function() {
-  this.particleCount = particleCount;
-  this.particleSize = particleSize;
-  this.fieldSize = fieldSize;
-  this.fieldForce = fieldForce;
-  this.noiseSpeed = noiseSpeed;
-  this.simplexOrPerlin = sORp;
-  this.trailLength = trailLength;
-  this.maxSpeed = maxSpeed;
-  this.hueBase = hueBase;
-  this.hueRange = hueRange;
-
-  this.change = function() {
-    particleSize = ui.particleSize;
-    fieldSize = ui.fieldSize;
-    fieldForce = ui.fieldForce;
-    noiseSpeed = ui.noiseSpeed;
-    maxSpeed = ui.maxSpeed;
-    hueBase = ui.hueBase;
-    hueRange = ui.hueRange;
-    fieldColor = ui.fieldColor;
-    ui.simplexOrPerlin?sORp=1:sORp=0;
-  }
-  
-  this.reset = function() {
-    particleCount = ui.particleCount;
-    reset();
-  }
-  
-  this.bgColor = function(){
-    trailLength = ui.trailLength;
-  }
-}();
+const MAXSPEED = 1.5;
+let width = 0;
+let height = 0;
 
 class Vector2 {
   constructor(x, y) {
@@ -90,7 +46,7 @@ class Particle {
     if (acceleration) this.acceleration.vAdd(acceleration);
     this.velocity.vAdd(this.acceleration);
     this.position.vAdd(this.velocity);
-    if (this.velocity.getMagnitude() > maxSpeed) this.velocity.setMagnitude(maxSpeed);
+    if (this.velocity.getMagnitude() > MAXSPEED) this.velocity.setMagnitude(MAXSPEED);
     this.acceleration.setMagnitude(0);
   }
 
@@ -108,79 +64,94 @@ class Particle {
   }
 }
 
-canvas = document.querySelector("#canvas");
-ctx = canvas.getContext("2d");
-reset();
-window.addEventListener("resize", reset);
+document.addEventListener("DOMContentLoaded", (event) => {
+  
+  let field = [];
+  let columns = 0;
+  let rows = 0;
+  let particles = [];
+  let noiseZ = 0;
+  const particleCount = 3000;
+  const particleSize = 0.85;
+  const fieldSize = 70;
+  const fieldForce = 0.15;
+  const noiseSpeed = 0.003;
+  const trailLength = 0.15;
+  const hueBase = 210;
+  const hueRange = 10;
+  const canvas = document.getElementById('flowfield');
+  const ctx = canvas.getContext('2d');
 
-function initParticles() {
-  particles = [];
-  let numberOfParticles = particleCount;
-  for (let i = 0; i < numberOfParticles; i++) {
-    let particle = new Particle(Math.random() * width, Math.random() * height);
-    particles.push(particle);
-  }
-}
-
-function initField() {
-  field = new Array(columns);
-  for (let x = 0; x < columns; x++) {
-    field[x] = new Array(rows);
-    for (let y = 0; y < rows; y++) {
-      let v = new Vector2(0, 0);
-      field[x][y] = v;
+  function initParticles() {
+    particles = [];
+    let numberOfParticles = particleCount;
+    for (let i = 0; i < numberOfParticles; i++) {
+      let particle = new Particle(Math.random() * width, Math.random() * height);
+      particles.push(particle);
     }
   }
-}
-
-function calcField() {
-  for (let x = 0; x < columns; x++) {
-    for (let y = 0; y < rows; y++) {
-      let angle = noise.simplex3(x / 20, y / 20, noiseZ) * Math.PI * 2;
-      let length = noise.simplex3(x / 40 + 40000, y / 40 + 40000, noiseZ) * fieldForce;
-      field[x][y].setMagnitude(length);
-      field[x][y].setAngle(angle);
+  
+  function initField() {
+    field = new Array(columns);
+    for (let x = 0; x < columns; x++) {
+      field[x] = new Array(rows);
+      for (let y = 0; y < rows; y++) {
+        let v = new Vector2(0, 0);
+        field[x][y] = v;
+      }
     }
   }
-}
-
-function reset() {
-  width = canvas.width = window.innerWidth;
-  height = canvas.height = window.innerHeight;
-  //ctx.strokeStyle = fieldColor;
-  noise.seed(Math.random());
-  columns = Math.round(width / fieldSize) + 1;
-  rows = Math.round(height / fieldSize) + 1;
-  initParticles();
-  initField();
-}
-
-function draw() {
-  requestAnimationFrame(draw);
-  calcField();
-  noiseZ += noiseSpeed;
-  drawBackground();
-  drawParticles();
-}
-
-function drawBackground() {
-  ctx.fillStyle = "rgba(0,0,0,"+ui.trailLength+")";
-  ctx.fillRect(0, 0, width, height);
-}
-
-function drawParticles() {
-  particles.forEach(particle => {
-    var ps = particle.fieldSize = Math.abs(particle.velocity.x + particle.velocity.y)*particleSize + 0.3;
-    ctx.fillStyle = "hsl("+(hueBase + particle.hue + ((particle.velocity.x + particle.velocity.y)*hueRange))+", 100%, 50%)";
-    ctx.fillRect(p.position.x, p.position.y, ps, ps);
-    let position = p.position.divide(fieldSize);
-    let displacement;
-    if (position.x >= 0 && position.x < columns && position.y >= 0 && position.y < rows) {
-      displacement = field[Math.floor(position.x)][Math.floor(position.y)];
+  
+  function calcField() {
+    for (let x = 0; x < columns; x++) {
+      for (let y = 0; y < rows; y++) {
+        let angle = noise.simplex3(x / 20, y / 20, noiseZ) * Math.PI * 2;
+        let length = noise.simplex3(x / 40 + 40000, y / 40 + 40000, noiseZ) * fieldForce;
+        field[x][y].setMagnitude(length);
+        field[x][y].setAngle(angle);
+      }
     }
-    particle.move(displacement);
-    particle.wrap();
-  });
-}
+  }
+  
+  function reset() {
+    width = canvas.width = window.innerWidth;
+    height = canvas.height = window.innerHeight;
+    noise.seed(Math.random());
+    columns = Math.round(width / fieldSize) + 1;
+    rows = Math.round(height / fieldSize) + 1;
+    initParticles();
+    initField();
+  }
+  
+  function draw() {
+    requestAnimationFrame(draw);
+    calcField();
+    noiseZ += noiseSpeed;
+    drawBackground();
+    drawParticles();
+  }
+  
+  function drawBackground() {
+    ctx.fillStyle = "rgba(1,14,20,"+trailLength+")";
+    ctx.fillRect(0, 0, width, height);
+  }
+  
+  function drawParticles() {
+    particles.forEach(particle => {
+      var ps = particle.fieldSize = Math.abs(particle.velocity.x + particle.velocity.y)*particleSize + 0.3;
+      ctx.fillStyle = "hsl("+(hueBase + particle.hue + ((particle.velocity.x + particle.velocity.y)*hueRange))+", 100%, 50%)";
+      ctx.fillRect(particle.position.x, particle.position.y, ps, ps);
+      let position = particle.position.divide(fieldSize);
+      let displacement;
+      if (position.x >= 0 && position.x < columns && position.y >= 0 && position.y < rows) {
+        displacement = field[Math.floor(position.x)][Math.floor(position.y)];
+      }
+      particle.move(displacement);
+      particle.wrap();
+    });
+  }
 
-draw();
+  reset();
+  window.addEventListener('resize', reset);
+  draw();
+});
